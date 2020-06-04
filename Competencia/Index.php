0086f1da66923f0befc10ff00c5c6a941e550ca8
Giver Cifuentes <gdcifuentes7@misena.edu.co>
2020-06-04 10:21:14 -0500

    Se agrega archivos usuarios

---
+++ Competencia/Index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1 align="center">Administrar competencias</h1>
+
+    <table align="center">
+        <thead>
+            <tr>
+            <td><a href="Vista/IngresarCompetencia.php">Ingresar</a></td>
+            <td><a href="Vista/ListarCompetencias.php">Listar</a></td>
+            </tr>
+            <tbody>
+            </tbody>
+        </thead>
+    </table>
+</body>
+</html>
--- Index.php
+++ Index.php
@@ -1,22 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>Document</title>
+     <link rel="stylesheet" href="css/estilosIndex.css"></link>
 </head>
 <body>
-    <h1 align="center">Administrar competencias</h1>
-
-    <table align="center">
-        <thead>
-            <tr>
-            <td><a href="Vista/IngresarCompetencia.php">Ingresar</a></td>
-            <td><a href="Vista/ListarCompetencias.php">Listar</a></td>
-            </tr>
-            <tbody>
-            </tbody>
-        </thead>
-    </table>
+     <h1 align="center">Ingreso</h1>
+    
+     <form action="Usuario/Controlador/ControladorUsuario.php" method="POST" align="center">
+          <div class="form-group">
+               <label for="">Usuario</label>
+               <input type="text" class="form-control" id="NombreUsuario" name="NombreUsuario" >
+          </div>
+          <div class="form-group">
+               <label for="">Password</label>
+               <input type="password" class="form-control" id="Contrasena" name="Contrasena">
+          </div>
+        
+          <button type="submit" name="Acceder" id="Acceder">Ingresar</button>
+     </form>
 </body>
 </html>
